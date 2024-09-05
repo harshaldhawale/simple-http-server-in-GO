7504d67c017c8387b991e424c3a3efb09a88a6ff
harshal <harshal.dhawale@gmail.com>
2024-09-05 16:49:42 +0530

    server can now listen on specified port

--- main.go
+++ main.go
@@ -7,5 +7,12 @@ import (
 )
 
 func main(){
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/",fileServer)
+
+	fmt.Printf("Starting server on port 8080 \n")
 	
+	if err := http.ListenAndServe(":8080", nil); err != nil{
+		log.Fatal(err)
+	}
 }
