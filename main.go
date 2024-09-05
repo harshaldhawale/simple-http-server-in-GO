efcf5b1a8447511208a32b95ab5f18069f6a1ffd
harshal <harshal.dhawale@gmail.com>
2024-09-05 17:14:20 +0530

    first basic http server using go is done

--- main.go
+++ main.go
@@ -6,9 +6,36 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter,r *http.Request){
+	if r.URL.Path != "/hello"{
+		http.Error(w,"404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET"{
+		http.Error(w, "method not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w,"hello!")
+}
+
+func formHandler(w http.ResponseWriter,r *http.Request){
+	if err := r.ParseForm(); err != nil{
+		fmt.Fprintf(w,"ParseForm() err: %v",err)
+		return
+	}
+	fmt.Fprintf(w, "Post request successful \n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w,"name = %s \n", name)
+	fmt.Fprintf(w,"address = %s \n", address)
+}
+
 func main(){
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/",fileServer)
+	http.HandleFunc("/hello",helloHandler)
+	http.HandleFunc("/form",formHandler)
 
 	fmt.Printf("Starting server on port 8080 \n")
 	
